08245dfba7d6fff7f70bf5bf520bb8417fdeb013
jaythaceo <jaythaceo@gmail.com>
2015-01-16 16:10:10 -0500

    [chore] Installed Devise gem

--- notenote/app/controllers/notes_controller.rb
+++ notenote/app/controllers/notes_controller.rb
@@ -22,11 +22,20 @@ class NotesController < ApplicationController
     end
   end
 
-  def update
+  def edit  
+  end
 
+  def update
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @note.destroy
+    redirect_to notes_path
   end
 
   private
